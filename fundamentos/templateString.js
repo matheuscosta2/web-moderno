034a51ca0cfdcaca954fc6c497cd45f89231a6d7
matheuscosta <matheuscostajgppo@gmail.com>
2020-11-26 21:32:42 -0300

    template string finalizado

--- fundamentos/templateString.js
+++ fundamentos/templateString.js
@@ -3,6 +3,9 @@ const concatenacao = 'Olá' + name + '!'
 const template = `Olá ${name}!`
 console.log(concatenacao, template)
 
-
 // expresions
 console.log(`1 + 1 = ${1 + 1}`)
+
+const up = text => text.toUpperCase()
+console.log(`Ei... ${up('cuidado')}`)
+
