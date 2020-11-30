640b00c099ce043c728995db3fe67240520d9d71
matheuscosta <matheuscostajgppo@gmail.com>
2020-11-30 20:52:56 -0300

    funcoes com e sem retorno

--- fundamentos/funcao1.js
+++ fundamentos/funcao1.js
@@ -1,1 +1,20 @@
 //Function without return
+
+function printSum(a, b) {
+    console.log(a + b)
+}
+
+printSum(2, 3)
+printSum(2)
+printSum(2, 10, 4, 5)
+printSum()
+
+//function with return
+
+function sum(a, b = 1) {
+    return a + b
+}
+
+console.log(sum(2, 3))
+console.log(sum(2))
+console.log(sum())
