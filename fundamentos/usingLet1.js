5ae0725bcaba35441970000d25a368e7b4ad07b8
matheuscosta <matheuscostajgppo@gmail.com>
2020-11-30 22:32:17 -0300

    Usando let e var em loops

---
+++ fundamentos/usingLet1.js
@@ -0,0 +1,7 @@
+var number = 1
+{
+    let number = 2
+    console.log('in =', number)
+}
+
+console.log('out =', number)
---
+++ fundamentos/usingVar2.js
@@ -0,0 +1,6 @@
+var number = 1 
+{
+    var number = 2
+    console.log('Dentro =', number)
+}
+console.log('out', number)
