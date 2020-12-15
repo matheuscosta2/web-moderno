659ebebaf8d6285c9cdeda18ac38ab7395385066
matheuscosta <matheuscostajgppo@gmail.com>
2020-12-15 18:54:21 -0300

    arrow function1

---
+++ funcao/arrowFunction.js
@@ -0,0 +1,16 @@
+let double= function (a) {
+    return 2 * a
+}
+
+double = (a)=> {
+    return 2 * a
+}
+
+double = a => 2 * a //return implicito
+
+let hello = function () { 
+    return 'Ola'
+}
+
+hello = () => 'Ola'
+hello = _ => 'Ola'
