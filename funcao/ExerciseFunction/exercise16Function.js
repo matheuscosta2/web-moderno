d30faf6b4e568cdd0d0bd84542f9bb0643b3f5fd
matheuscosta <matheuscostajgppo@gmail.com>
2020-12-21 21:32:02 -0300

    exercise 16

--- funcao/ExerciseFunction/exercise16Function.js
+++ funcao/ExerciseFunction/exercise16Function.js
@@ -1,4 +1,4 @@
-/*16)​ Utilizando a estrutura do Switch faça um programa que simule uma calculadora básicaO programa recebecomo parâmetros dois valores numéricos e uma string referente à operação e a realize com os valores
+/*16)​Utilizando a estrutura do Switch faça um programa que simule uma calculadora básicaO programa recebecomo parâmetros dois valores numéricos e uma string referente à operação e a realize com os valores
 numéricos na ordem que foram inseridos. Por exemplo: calculadora (2, ‘+’, 3). A função efetuará a soma de 2 e3. Dica: Os sinais das operações são: ‘+’. ‘-’, ‘*’ e ‘/’.  Crie um caso default para operações inválidas.*/
 const Calculator = (number1, operation, number2) => {
   switch (operation) {
@@ -19,4 +19,4 @@ const Calculator = (number1, operation, number2) => {
   }
 };
 
-Calculator(1, "+", 1);
+Calculator(1, "+", 1); 
