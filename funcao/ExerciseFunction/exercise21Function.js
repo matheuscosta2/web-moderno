00001a8b2380bc1a72988af268c97baf7ad13608
matheuscosta <matheuscostajgppo@gmail.com>
2020-12-26 23:32:47 -0300

    Exercise 21

---
+++ funcao/ExerciseFunction/exercise21Function.js
@@ -0,0 +1,18 @@
+const value = 100 
+
+const healthPlan = (age) => {
+
+    if(age < 0) {
+        return 'Error'
+    }else if (age < 10) {
+        return value + 80
+    }else if (age < 30) {
+        return value + 50
+    }else if (age < 60) {
+        return value + 95
+    }else {
+        return value + 130
+    }
+}
+
+console.log(healthPlan(9))
